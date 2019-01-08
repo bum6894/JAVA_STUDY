2a5c5ce6b83db36a6b22ec752c00e5c175449e82
admin <admin@10.137.138.110>
2019-01-08 16:46:03 +0900

    update 2

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,8 +1,11 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	/*
+	 * 가나다라마바사아자차카타파하가갸거겨고교구규그기
+	 */
 	public static void main(String[] args) {
+		//코멘트
 		System.out.println("Hello World");
 		System.out.println("안녕");
 	}

10002201f35cc870a79d81f8eb9b74643604209b
Administrator <Administrator@MSDN-SPECIAL>
2019-01-11 00:41:00 +0900

    homework clear

--- src/icehs/science/chapter04/SalaryTest.java
+++ src/icehs/science/chapter04/SalaryTest.java
@@ -3,8 +3,21 @@ package icehs.science.chapter04;
 public class SalaryTest {
 
 	public static void main(String[] args) {
-		aaaaa
-
+		int salary = 1000000;
+		
+		int yearSalary = salary * 12;
+		int bonus = salary * 20/100 * 4;
+		
+		int salaryTax = salary * 10/100;
+		int bonusTax = bonus * 55/1000;
+		
+		int yearSalaryMinusTax =  yearSalary - 12 * salaryTax;
+		int BonusMinusTax = bonus - bonusTax;
+		
+		System.out.println("연봉 : " + yearSalary + ", 세후 연봉 : " + yearSalaryMinusTax);
+		System.out.println("보너스 : " + bonus + ", 세후 보너스 : " + BonusMinusTax);
+		System.out.println("지급액 : " + (yearSalaryMinusTax + BonusMinusTax));
+		
 	}
 
 }
--- src/icehs/science/chapter04/SeperateNumberTest.java
+++ src/icehs/science/chapter04/SeperateNumberTest.java
@@ -10,7 +10,7 @@ public class SeperateNumberTest {
 		int number1 = (number % 100) % 10;
 		
 		System.out.println("숫자 : " + number);
-		System.out.println("백의 자리 수 : " + (number100));
+		System.out.println("백의 자리 수 : " + number100);
 		System.out.println("십의 자리 수 : " + number10);
 		System.out.println("일의 자리 수 : " + number1);
 

4ac570e6358b2c894ab88587333ec013a9fa2feb
admin <admin@10.137.138.110>
2019-01-10 18:11:21 +0900

    chapter 05

---
+++ src/icehs/science/chapter05/ConditionIfEx.java
@@ -0,0 +1,15 @@
+package icehs.science.chapter05;
+
+public class ConditionIfEx {
+	
+	public static void main(String[] args) {
+		int age = 2;
+		
+		if (age >= 20) {
+			System.out.println("입장하세요.");
+			}
+		if (age <20) {
+			System.out.println("입장 불가합니다.");
+			}
+	}
+}

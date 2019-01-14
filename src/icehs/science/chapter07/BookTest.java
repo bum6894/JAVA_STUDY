e6a47740fed7caec558ee8ca12d546374fd72350
admin <admin@10.137.138.110>
2019-01-14 18:44:59 +0900

    chapter 06

---
+++ src/icehs/science/chapter07/BookTest.java
@@ -0,0 +1,12 @@
+package icehs.science.chapter07;
+
+public class BookTest {
+
+	public static void main(String[] args) {
+		Book book1 = new Book();
+		Book book2 = new Book("해리포터", 19000);
+		
+
+	}
+
+}
